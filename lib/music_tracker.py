6586eaa443106c542a88c6e1afbbc68ab3e84c7d
patrickskipworth <pskipworth@fastmail.com>
2024-02-02 16:05:58 +0000

    Final tests complete

--- lib/music_tracker.py
+++ lib/music_tracker.py
@@ -4,6 +4,13 @@ class MusicTracker():
         self.tracklist = []
 
     def add(self, track):
+        values = track.values()
+        for value in values:
+            if type(value) != str:
+                raise Exception('Can\'t add track information')
+            elif value == '':
+                raise Exception('Can\'t add empty track name')    
+        
         self.tracklist.append(track)
     
     def get_tracklist(self):
